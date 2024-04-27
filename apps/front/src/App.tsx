6d42cd02942f2b57b4b7903d4bd240c262626805
Isur <bednarczykartur96@gmail.com>
2024-04-27 21:30:08 +0200

    Move tailwind config to another package

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-5xl text-red-500 font-bold underline">
+    <h1 className="text-5xl text-primary bg-backgroundColor font-bold underline">
       Vite + React + Tailwind + Typescript
     </h1>
   );
---
+++ packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+// We want each package to be responsible for its own content.
+const config: Omit<Config, "content"> = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#d65d0e",
+        backgroundColor: "#282828",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
