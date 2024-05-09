90b131cd312627f044f14f88bbefb3c379aabfe3
Isur <bednarczykartur96@gmail.com>
2024-05-09 22:28:35 +0200

    feature: example package with build/dev

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { helloWorld } from "@repo/hello-world-package";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World!";
+    return helloWorld();
   }
 }
