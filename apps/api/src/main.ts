8f00956bf863e5636d5807587f09933fb4e2d99a
Isur <bednarczykartur96@gmail.com>
2024-06-08 21:01:07 +0200

    Installed cookie parer

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ConfigService } from "@nestjs/config";
 import { Logger } from "@nestjs/common";
+import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 import { AppConfig } from "./config/app.config";
 import { CONFIGKEYS } from "./config/configKeys";
@@ -9,6 +10,8 @@ import { CONFIGKEYS } from "./config/configKeys";
 async function bootstrap() {
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   const configService = app.get(ConfigService);
   const config = configService.get<AppConfig>(CONFIGKEYS.APP);
