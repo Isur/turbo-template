c7139dc36450269cdd342874b0aa7891184faecf
Isur <bednarczykartur96@gmail.com>
2024-06-19 22:20:51 +0200

    Sentry

--- apps/front/src/devtools.tsx
+++ apps/front/src/devtools.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import * as Sentry from "@sentry/react";
+
+process.env.NODE_ENV === "production" &&
+  Sentry.init({
+    dsn: "https://1b02df46d2e9e707dc2950d7a24bab29@o4507460359553024.ingest.de.sentry.io/4507460364861520",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: [/^\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null
--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,19 +1,33 @@
 import path from "path";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    sentryVitePlugin({
+      org: "artur-bednarczyk",
+      project: "javascript-react",
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     proxy: {
       "/api": "http://localhost:3000",
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
