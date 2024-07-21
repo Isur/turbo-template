dfb96feb1dd5333945566534ba261024b27e3922
Isur <bednarczykartur96@gmail.com>
2024-07-21 20:11:17 +0200

    Nestjs sentry

--- apps/api/src/config/sentry.config.ts
+++ apps/api/src/config/sentry.config.ts
@@ -4,6 +4,7 @@ import { CONFIGKEYS } from "./configKeys";
 export interface SentryConfig {
   host: string;
   projects: Array<string>;
+  dsn: string;
 }
 
 export default registerAs(
@@ -11,5 +12,6 @@ export default registerAs(
   (): SentryConfig => ({
     host: process.env.SENTRY_HOST || "",
     projects: process.env.SENTRY_PROJECTS.split(",") || [],
+    dsn: process.env.SENTRY_DSN || "",
   })
 );
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,28 +1,49 @@
-import { NestFactory } from "@nestjs/core";
+import {
+  BaseExceptionFilter,
+  HttpAdapterHost,
+  NestFactory,
+} from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ConfigService } from "@nestjs/config";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import cookieParser from "cookie-parser";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
+import * as Sentry from "@sentry/nestjs";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import { AppModule } from "./app.module";
 import { AppConfig } from "./config/app.config";
 import { CONFIGKEYS } from "./config/configKeys";
 import { CustomHttpExceptionFilter } from "./exceptions/httpException.filter";
+import { SentryConfig } from "./config";
 
 async function bootstrap() {
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+  const config = configService.get<AppConfig>(CONFIGKEYS.APP);
+  const sentryConfig = configService.get<SentryConfig>(CONFIGKEYS.SENTRY);
+
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  Sentry.init({
+    dsn: sentryConfig.dsn,
+    environment: config.env_name ?? "production",
+    integrations: [nodeProfilingIntegration()],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
 
   app.use(cookieParser());
   app.useBodyParser("text");
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomHttpExceptionFilter());
-
-  const configService = app.get(ConfigService);
-  const config = configService.get<AppConfig>(CONFIGKEYS.APP);
 
   app.setGlobalPrefix("api");
 
