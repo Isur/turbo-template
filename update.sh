dc460ac52cca092aa45ef65adfd5ba5ac3ba1fac
Isur <bednarczykartur96@gmail.com>
2024-07-28 14:00:32 +0200

    update packages

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update Everything
+pnpm update
+
+(cd ./apps/api; pnpm update)
+(cd ./apps/front; pnpm update)
+(cd ./packages/mailer; pnpm update)
+(cd ./packages/api-client; pnpm update)
+(cd ./packages/drizzle-connector; pnpm update)
+(cd ./packages/hello-world-package; pnpm update)
