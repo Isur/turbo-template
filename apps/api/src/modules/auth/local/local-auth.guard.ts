5a0c50b5f873ddc908fdb7f175c026d67c293f76
Isur <bednarczykartur96@gmail.com>
2025-01-18 21:08:55 +0100

    fix: local authguard with correct error

--- apps/api/src/modules/auth/local/local-auth.guard.ts
+++ apps/api/src/modules/auth/local/local-auth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { UserWithoutPassword } from "../auth.service";
+import { UnauthorizedException } from "../auth.errors";
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard("local") {}
+export class LocalAuthGuard extends AuthGuard("local") {
+  handleRequest<TUser = UserWithoutPassword>(
+    err: any,
+    user: TUser,
+    info: any,
+    _context: ExecutionContext,
+    _status?: any
+  ): TUser {
+    if (err || !user || info) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
